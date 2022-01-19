4241575ba388ae0c72b9a57384881389ce781ab2
Maen Artimy <artimym@gmail.com>
2022-01-19 14:10:55 -0400

    modified caching to add ttl

--- read_sensors.py
+++ read_sensors.py
@@ -10,7 +10,7 @@ import influxdb
 import streamlit as st
 
 
-#@st.cache(allow_output_mutation=True)
+@st.cache(allow_output_mutation=True, ttl=0)
 def load_dataframe(host, dbname, user, password):
     port = 8086
 
